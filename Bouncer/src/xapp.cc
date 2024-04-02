406941101003f8e1fff8080c1064414cfbf901cc
gckopper <gabriel@gck.dev.br>
2024-04-02 16:33:34 -0300

    Revert back to when the mesurement was taken as time between subcription request and subcription response
    This reverts commit 1540a57651be0dfe773aca2445688b7f3aa51897.

--- Bouncer/src/xapp.cc
+++ Bouncer/src/xapp.cc
@@ -224,6 +224,9 @@ inline void Xapp::subscribe_request(string meid, jsonn subObject, const std::chr
 			std::string tmp;
 			tmp = jsonObject[U("SubscriptionId")].as_string();
 			subscription_map.emplace(std::make_pair(meid, tmp));
+            const auto t2 = std::chrono::high_resolution_clock::now();
+            const int int_ms = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+            std::cout << "\nFinished subscription flow: " << int_ms.count() << "\n";
 	});
 
 	try
